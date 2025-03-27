9266b61980e45aa1471b023741c1a58e971cd33e
Ryan <ryan.silva@showtecnologia.com>
2025-03-26 21:21:30 -0300

    Added model and routes files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
 const db = require('./db/connection')
+const bodyParser = require("body-parser")
 
 const PORT = 3300
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.listen(PORT,function(){
     console.log(`O express está rodando na porta ${PORT}`)
@@ -21,4 +24,7 @@ db.authenticate()
 app.get('/',(req,res) => {
     res.send("Está funcionando")
 })
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'))
 
